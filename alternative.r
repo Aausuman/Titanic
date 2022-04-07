e136053efd7b5e5061a50b660c270aef80d5d338
Aausuman Deep <aausuman.deep@gmail.com>
2022-04-07 01:26:06 +0100

    Updates to the R script

--- alternative.r
+++ alternative.r
@@ -1,6 +1,14 @@
 # This file is an alternative approach to the python notebook
 # Attempting to achieve the same result
+install.packages("randomForest")
+library(randomForest)
 
 train_data = read.csv("/Users/aausumandeep/Documents/Gits/Titanic/titanic_data/train.csv") # nolint
 typeof(train_data)
+head(train_data)
+
+test_data = read.csv("/Users/aausumandeep/Documents/Gits/Titanic/titanic_data/test.csv") # nolint
+
+sapply(train_data, class)
+train_data <- transform(train_data, Survived = as.factor(Survived))
 
