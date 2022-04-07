9a54a25dd1da16c258ddbda92ecb81e9cd41d791
Aausuman Deep <aausuman.deep@gmail.com>
2022-04-07 01:07:22 +0100

    Updates

--- alternative.r
+++ alternative.r
@@ -1,6 +1,6 @@
 # This file is an alternative approach to the python notebook
 # Attempting to achieve the same result
 
-train_data = read.csv("/Users/aausumandeep/Documents/Gits/Titanic/titanic_data/train.csv")
+train_data = read.csv("/Users/aausumandeep/Documents/Gits/Titanic/titanic_data/train.csv") # nolint
 typeof(train_data)
 
